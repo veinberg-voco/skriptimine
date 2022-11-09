11d9c1e6a80e1ecec63cd1d8f921ff5e088529ca
kevin veinberg <kevin.veinberg@voco.ee>
2022-11-09 00:54:19 -0800

    skriptiminePowershell

---
+++ skriptimine.ps1
@@ -0,0 +1,4 @@
+﻿$KasutjaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+# Loome kasutaja jaoks parool
+# Loome kasutaja
+New-LocalUser 'kasutaja1' -Password $KasutjaParool -FullName 'Esimene kasutaja' -Description 'local account' 
