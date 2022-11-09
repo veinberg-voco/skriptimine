d2236926b6bb3c1bcf1aaa40862b051e9b3edc27
kevin veinberg <kevin.veinberg@voco.ee>
2022-11-09 01:20:57 -0800

    Lokaalne kasutaja käsurea parameetritega

---
+++ kasutaja_loomine_parameetritega.ps1
@@ -0,0 +1,10 @@
+﻿# vajalikud parameetrid
+param(
+$kasutajanimi,
+$Taisnimi,
+$KontoKirjeldus
+)
+# Loome kasutaja jaoks parool
+$KasutjaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+# Loome kasutaja
+New-LocalUser $kasutajanimi -Password $KasutjaParool -FullName $Taisnimi -Description $KontoKirjeldus
