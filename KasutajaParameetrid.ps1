d71dedfe626246ce399bad541aa2668976441566
veinberg-voco <114643514+veinberg-voco@users.noreply.github.com>
2023-03-27 20:19:55 +0300

    ParameetridPaigas

---
+++ KasutajaParameetrid.ps1
@@ -0,0 +1,10 @@
+﻿#kontroll parameetrite arvule
+if($args.Count -ne 3){
+echo "kasutusjuhend: .\skriptifailinimi.ps1, kasutajanimi, täisnimi, kirjeldus"
+}else{
+    $kasutajanimi = $args[0]
+    $täisnimi = $args[1]
+    $kontokirjeldus = $args[2]
+    $KasutajaParool = ConvertTo-SecureString "Krissu423423" -AsPlainText -Force
+    New-LocalUser "$kasutajanimi" -Password $KasutajaParool -Fullname "$täisnimi" -description "$kontokirjeldus"
+    }
