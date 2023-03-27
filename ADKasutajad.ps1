54e1e0f2ab3135c1506475d365af5ab7862860ce
veinberg-voco <114643514+veinberg-voco@users.noreply.github.com>
2023-03-27 20:14:51 +0300

    Kasutajad

---
+++ ADKasutajad.ps1
@@ -0,0 +1,4 @@
+﻿FirstName;LastName;SamAccountName;Department;Role;Password,
+Kadi;Tamm;kaditamm;IKT;opilane;Parool1!
+Terje;Lepp;terjelepp;IKT;opilane;Parool1!
+Tõnis;Kokk;tanelkokk;IKT;opilane;Parool1!
