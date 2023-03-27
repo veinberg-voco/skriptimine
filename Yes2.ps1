bd802d2ecb67d8d37bcbe63c96cc9f2b13f0c91e
veinberg-voco <114643514+veinberg-voco@users.noreply.github.com>
2023-03-27 20:12:31 +0300

    Delete Yes2.ps1

--- Yes2.ps1
+++
@@ -1,4 +0,0 @@
-﻿FirstName;LastName;SamAccountName;Department;Role;Password,
-Kadi;Tamm;kaditamm;IKT;opilane;Parool1!
-Terje;Lepp;terjelepp;IKT;opilane;Parool1!
-Tõnis;Kokk;tanelkokk;IKT;opilane;Parool1!
